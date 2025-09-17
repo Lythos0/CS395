df015b7d4107fa11604bafc6103c7cae6a823c14
Cortney M <123661123+Lythos0@users.noreply.github.com>
2025-09-17 18:05:53 -0400

    Create task01.cpp

---
+++ Labs/Lab02/task01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+bool isSet(int arr[], int size){
+
+for(int i = 0; i < size; i++){
+for(int j = i + 1; j < size; j++){
+  if(arr[i] == arr[j]){
+return false;
+  }
+}
+}
+return true;
+}
