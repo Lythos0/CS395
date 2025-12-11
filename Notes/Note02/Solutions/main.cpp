b2c75a2a2adc96c175dcaab165cb11b60d7981a6
ReidConsole <jermainereid@mec.science>
2025-12-10 19:23:12 -0500

    Notes Update

--- Notes/Note02/Solutions/main.cpp
+++ Notes/Note02/Solutions/main.cpp
@@ -21,10 +21,59 @@ int main()
     std::cout << "Remove Epsilon Rules:\n";
     std::cout << A << "\n";
 
+    /*
+    C -> S *
+    S -> AB
+    A -> 11A
+    A -> 11
+    B -> 0
+    B -> 00B
+    S -> B *
+    -------------------
+    C -> S | AB | B 
+    S -> AB | B | 0 | OOB
+    A -> 11A | 11
+    B -> 0 | 00B
+    ------------------- 
+    C -> AB | B * 
+    S -> AB | 0 | OOB
+    A -> 11A | 11
+    B -> 0 | 00B
+    -------------------
+    C -> AB | B | 0 | 00B 
+    S -> AB | 0 | OOB
+    A -> 11A | 11
+    B -> 0 | 00B
+    --------------------
+    C -> AB | 0 | 00B 
+    S -> AB | 0 | OOB
+    A -> 11A | 11
+    B -> 0 | 00B
+    */
     tc::RemoveUnits(A,a);
     std::cout << "Remove Unit Rules:\n";
     std::cout << A << "\n";
 
+    /*
+    Lexical Rules
+    C -> AB | 0 | 00B 
+    S -> AB | 0 | 00B
+    A -> 11A | 11
+    B -> 0 | 00B
+    --------------------
+    C -> AB | 0 | DDB 
+    S -> AB | 0 | DDB
+    A -> 11A | 11
+    B -> 0 | DDB
+    D -> 0
+    --------------------
+    C -> AB | 0 | DDB 
+    S -> AB | 0 | DDB
+    A -> EEA | EE
+    B -> 0 | DDB
+    D -> 0
+    E -> 1
+    */
     tc::ConstructLexicals(A,a);
     std::cout << "Construct Lexical Rules:\n";
     std::cout << A << "\n";
